ede69d286cf21df0a034269c2324cb9d4d684031
@OsvaldoMoraes61 <110261126+OsvaldoMoraes61@users.noreply.github.com>
2023-03-08 12:28:36 -0300

    Initial commit

---
+++ olamundo.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang=pt-br>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro exemplo</title>
+</head>
+<body>
+    <h1>Testando o PHP</h1>
+    <?php
+        echo '<h2>  Olá, Mundão </h2>'
+
+    ?>
+</body>
+</html>
+
